46c67cd9bdaed109a116436238556288ea13aa30
davidpage876 <davidpage876@gmail.com>
2021-11-13 20:05:50 +1000

    Create project and add Task panel UI

---
+++ Task Time Counter 2/Task.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Task_Time_Counter_2
+{
+    public sealed partial class Task : UserControl
+    {
+        public Task()
+        {
+            this.InitializeComponent();
+        }
+
+        private void textBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TimeEdit_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TimeBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
