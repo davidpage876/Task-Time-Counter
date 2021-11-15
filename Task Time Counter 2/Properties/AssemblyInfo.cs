8e6865d9cec89d1486f4869005f4022a87b4c6fc
davidpage876 <davidpage876@gmail.com>
2021-11-15 21:02:45 +1000

    Correct assembly name

--- Task Time Counter 2/Properties/AssemblyInfo.cs
+++ Task Time Counter 2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Task Time Counter 2")]
+[assembly: AssemblyTitle("Task Time Counter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task Time Counter 2")]
+[assembly: AssemblyProduct("Task Time Counter")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
